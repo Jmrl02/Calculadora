fee0761e7c78ae6a33e5643d784f1da781f5698c
Jmrl02 <aluno23037@ipt.pt>
2022-03-29 17:24:16 +0100

    aula 2

--- Vets/Vets/Data/ApplicationDbContext.cs
+++ Vets/Vets/Data/ApplicationDbContext.cs
@@ -1,13 +1,34 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Vets.Models;
 
 namespace Vets.Data
 {
+
+
+
+
+
+
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Animais> Animais { get; set; }
+
+        public DbSet<Veterinarios> Veterinarios { get; set; }
+
+        public DbSet<Donos> Donos { get; set; }
+
+
+        public DbSet<Consultas> Consultas { get; set; }
+
+
+
+
+
     }
 }
